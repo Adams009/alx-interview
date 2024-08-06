daee815d6b6fad085b82eb1a9a816f5c23c37359
Adams009 <olaadam003@gmail.com>
2024-08-06 10:35:24 -0700

    add task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,31 @@
 #!/usr/bin/node
 
 const request = require('request');
+const { promisify } = require('util');
+const requestAsync = promisify(request);
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
+async function getMovieCharacters(movieId) {
+  try {
+    // Fetch the movie data
+    const { body: movieBody } = await requestAsync(`https://swapi-api.hbtn.io/api/films/${movieId}`);
+    const { characters } = JSON.parse(movieBody);
+
+    // Fetch character data in parallel
+    const characterPromises = characters.map(characterUrl => requestAsync(characterUrl).then(response => JSON.parse(response.body).name));
+    const characterNames = await Promise.all(characterPromises);
+
+    // Print character names
+    characterNames.forEach(name => console.log(name));
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Get the Movie ID from command-line arguments and execute the function
+const movieId = process.argv[2];
+if (movieId) {
+  getMovieCharacters(movieId);
+} else {
+  console.error('Please provide a Movie ID.');
+}
